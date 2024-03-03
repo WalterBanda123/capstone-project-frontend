ed0af4f33064ab37ff29b2e5f0a34e03abea8257
Walter Banda <walterbanda98@gmail.com>
2024-03-04 00:34:20 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "landing-page", pathMatch: 'full' },
+  { path: "landing-page", component: LandingPageComponent },
+  { path: "dashboard", component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
